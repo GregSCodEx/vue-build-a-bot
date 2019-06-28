8f8f40af0f59194ef5d48e244e50ed0bc0801473
Grzegorz Sala <gregscodex@mixbox.pl>
2019-06-28 15:28:25 +0200

    Module 3: content into a child

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open = !open">&#x25B2; Collaps</span>
+      <span v-if="!open" @click="open = !open">&#x25Bc; Expand</span>
+    </div>
+    <slot v-if="open">
+      <div>Default</div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #bbbbbb;
+  padding: 3px;
+  cursor: pointer;
+}
+</style>
